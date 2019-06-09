9eac6ad97fec02e41075798d996dbc7955a41956
LP Boucher <lp_boucher@hotmail.com>
2019-06-09 22:21:13 +0200

    created initial routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
-import Navbar from './components/container/Navbar/Navbar';
+import Navbar from './components/presentational/Navbar/Navbar';
 import Home from './components/container/Home/Home';
 import Shop from './components/container/Shop/Shop';
 import SubscriptionDetails from './components/container/Subscriptions/SubscriptionDetails';
@@ -11,7 +11,8 @@ import Wholesale from './components/container/Wholesale/Wholesale';
 import Contact from './components/container/Contact/Contact';
 import Blog from './components/container/Blog/Blog';
 import Account from './components/container/Account/Account';
-import Cart from './components/container/Cart/Cart';
+import Cart from './components/container/Cart/CartContainer';
+import ProductDetailsContainer from './components/container/Products/ProductDetailsContainer';
 
 class App extends Component {
   render() {
@@ -20,7 +21,6 @@ class App extends Component {
         <Navbar></Navbar>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/shop" component={Shop} />
           <Route path="/subscriptions" component={SubscriptionDetails} />
           <Route path="/about" component={About} />
           <Route path="/wholesale" component={Wholesale} />
@@ -28,6 +28,8 @@ class App extends Component {
           <Route path="/blog" component={Blog} />
           <Route path="/account" component={Account} />
           <Route path="/cart" component={Cart} />
+          <Route path="/shop/:slug" component={ProductDetailsContainer} />
+          <Route path="/shop" component={Shop} />
         </Switch>
       </Fragment>
     );
