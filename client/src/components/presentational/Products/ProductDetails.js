f610cde465ae7ff11c4a07adaed7e34a43a0b628
LP Boucher <lp_boucher@hotmail.com>
2019-08-06 18:07:32 +0200

    descriptions

--- client/src/components/presentational/Products/ProductDetails.js
+++ client/src/components/presentational/Products/ProductDetails.js
@@ -4,12 +4,11 @@ import { Form, Field } from 'react-final-form';
 import { SelectAdapter } from '../../utils/Forms/FormHelpers';
 
 import IconedExplanation from '../../utils/IconedExplanation';
-import ProductSpecs from './ProductSpecs';
 
-import { Heading, Text, Grid, Button, Box } from 'grommet';
+import { Heading, Text, Grid, Button, Box, Tabs, Tab } from 'grommet';
 import { Grow } from 'grommet-icons';
 
-const ProductDetails = ({id, name, description, product_type, price, link, slug, main, addToCart}) => {
+const ProductDetails = ({id, name, description, product_type, details, price, link, slug, main, addToCart}) => {
     const onSubmit = values => {
         console.log(values);
     }
@@ -17,45 +16,56 @@ const ProductDetails = ({id, name, description, product_type, price, link, slug,
         <Fragment>
             <Heading level={1}>{name}</Heading>
             <Heading level={3} margin={{"bottom": "medium"}} size="small">Nueva Segovia, Nicaragua | Espresso Beans</Heading>
-            <Text>The Esperanza is from the region of Nueva Segovia in Nicaragua. You can expect floral, tea-like acidity, brown-sugar tasting notes. This single origin coffee is an Espresso Roast, perfect with milk to balance the acidity.</Text>
-            <Text>{description}</Text>
-            {product_type === 'coffee' &&
-            <Fragment>
-                <IconedExplanation 
-                    icon={<Grow color="plain" size="medium" />}
-                    description={"These beans were harvested in the highest of the peralta farms in Nicaragua (Santa Maria de Lourdes)."}
-                    spacing={{vertical: 'medium'}}
-                    size={"xsmall"}
-                />
-                <hr/>
-                <Grid columns="50%" rows="50%">
-                <IconedExplanation 
-                    icon={<Grow color="plain" size="medium" />}
-                    description={"Red Honey, Nueva Segovia, Nicaragua"}
-                    spacing={{vertical: 'medium'}}
-                    size={"xsmall"}
-                />
-                <IconedExplanation 
-                icon={<Grow color="plain" size="medium" />}
-                description={"Recommended as a Cappuccino"}
-                spacing={{vertical: 'medium'}}
-                size={"xsmall"}
-                />
-                <IconedExplanation 
-                    icon={<Grow color="plain" size="medium" />}
-                    description={"Floral, Brown Sugar, Teal-like acidity, clean"}
-                    spacing={{vertical: 'medium'}}
-                    size={"xsmall"}
-                />
-                <IconedExplanation 
-                icon={<Grow color="plain" size="medium" />}
-                description={"100% recyclable bag, recyclable box"}
-                spacing={{vertical: 'medium'}}
-                size={"xsmall"}
-                />
-                </Grid>
-            </Fragment>
-            }
+            <Tabs justify="start">
+                <Tab title="Description">
+                    <Text>{description}</Text>
+                    {product_type === 'coffee' &&
+                    <Fragment>
+                        <IconedExplanation 
+                            icon={<Grow color="plain" size="medium" />}
+                            description={"These beans were harvested in the highest of the peralta farms in Nicaragua (Santa Maria de Lourdes)."}
+                            spacing={{vertical: 'medium'}}
+                            size={"xsmall"}
+                        />
+                        <hr/>
+                        <Grid columns="50%" rows="50%">
+                        <IconedExplanation 
+                            icon={<Grow color="plain" size="medium" />}
+                            description={"Red Honey, Nueva Segovia, Nicaragua"}
+                            spacing={{vertical: 'medium'}}
+                            size={"xsmall"}
+                        />
+                        <IconedExplanation 
+                        icon={<Grow color="plain" size="medium" />}
+                        description={"Recommended as a Cappuccino"}
+                        spacing={{vertical: 'medium'}}
+                        size={"xsmall"}
+                        />
+                        <IconedExplanation 
+                            icon={<Grow color="plain" size="medium" />}
+                            description={"Floral, Brown Sugar, Teal-like acidity, clean"}
+                            spacing={{vertical: 'medium'}}
+                            size={"xsmall"}
+                        />
+                        <IconedExplanation 
+                        icon={<Grow color="plain" size="medium" />}
+                        description={"100% recyclable bag, recyclable box"}
+                        spacing={{vertical: 'medium'}}
+                        size={"xsmall"}
+                        />
+                        </Grid>
+                    </Fragment>
+                    }
+                </Tab>
+                {product_type === "equipment" &&
+                <Tab title="Details">
+                    {details.split(";").map(detail => (
+                        <Text>{`- ${detail}\n`}</Text>
+                    ))}
+                </Tab>
+                }
+            </Tabs>
+            
             <Form
                 onSubmit={onSubmit}
                 render={({ handleSubmit, form, submitting, invalid, pristine, values, errors }) => (
@@ -73,7 +83,6 @@ const ProductDetails = ({id, name, description, product_type, price, link, slug,
                     </form>
             )}
             />
-            <ProductSpecs />
         </Fragment>
     );
 };
