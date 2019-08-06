d6d2610b192abdb4d581c6c8da70cd1cd41e1655
LP Boucher <lp_boucher@hotmail.com>
2019-08-06 18:07:17 +0200

    added out-of-stock handling in UI

--- client/src/components/presentational/Products/ProductCard.js
+++ client/src/components/presentational/Products/ProductCard.js
@@ -4,19 +4,30 @@ import { withRouter } from 'react-router-dom';
 
 import ProductCardInfo from './ProductCardInfo';
 
-import { Box, Image } from 'grommet';
+import { Box, Image, Stack, Text } from 'grommet';
 
 const WithHover = styled(Box)`
+    opacity: ${props => props.isManaged ? 0.5 : 1};
+    cursor: pointer;
+    position: relative;
     &:hover {
         opacity: 0.5;
     }
+`
+const OutOfStock = styled(Stack)`
+    position: absolute;
 `
 
 const ProductCard = withRouter(({product, thumb, addToCart, history}) => {
     return (
         <Fragment>
-            <WithHover height="75%" width="100%" onClick={() => history.push(`/shop/${product.slug}`)} style={{cursor: "pointer"}}>
+            <WithHover height="75%" width="100%" onClick={() => history.push(`/shop/${product.slug}`)} isManaged={product.manage_stock}>
                 <Image fit="contain" src={`${thumb.link? thumb.link.href : ""}`}/>
+                {product.manage_stock && product.meta.stock.level < 1 &&
+                    <OutOfStock fill>
+                        <Text>Sold Out</Text>
+                    </OutOfStock>
+                }
             </WithHover>
             <Box height="25%">
                 <ProductCardInfo 
@@ -24,6 +35,7 @@ const ProductCard = withRouter(({product, thumb, addToCart, history}) => {
                     name={product.name}
                     price={product.price}
                     add={addToCart}
+                    outOfStock={product.manage_stock && product.meta.stock.level < 1}
                     />
             </Box>      
         </Fragment>
--- client/src/components/presentational/Products/ProductCardInfo.js
+++ client/src/components/presentational/Products/ProductCardInfo.js
@@ -12,20 +12,22 @@ const CURRENCY_SYMBOLS = {
 }
 
 const WithHover = styled(Box)`
+    cursor: pointer;
+    border-radius: 4px;
     &:hover {
-        background-color: #a96c35;
+        background-color: ${props => props.outOfStock ? '#dcddde' : '#a96c35'};
     }
 `
 
-const ProductCardInfo = ({id, name, price, add}) => {
+const ProductCardInfo = ({id, name, price, add, outOfStock}) => {
     return (
         <Box height="100%" width="100%" direction="row" align="center" justify="around">
             <Box flex="grow">
                 <Text textAlign="center" weight="bold" style={{textTransform: 'uppercase'}}>{name}</Text>
                 <Text textAlign="center" color="grey">{`${CURRENCY_SYMBOLS[price[0].currency]}${price[0].amount/100}`}</Text>
             </Box>
-            <WithHover margin="small" pad="xsmall" background="mainDark" style={{cursor: 'pointer', borderRadius: '4px'}}>
-                <Add size="small" onClick={() => add(id, '1')}/>
+            <WithHover margin="small" pad="xsmall" background={`${outOfStock ? 'darkGrey' : 'mainDark'}`} outOfStock={outOfStock}>
+                <Add size="small" onClick={!outOfStock ? () => add(id, '1') : null}/>
             </WithHover>
         </Box>
     );
