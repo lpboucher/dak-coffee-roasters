b3c73ef1d49a2da2a916cf7cde499c8ea50dd7e3
LP Boucher <lp_boucher@hotmail.com>
2019-09-15 10:15:48 +0200

    fix social media link in footer

--- client/src/components/presentational/Footer/FooterDescription.js
+++ client/src/components/presentational/Footer/FooterDescription.js
@@ -10,8 +10,8 @@ const FooterDescription = ({t, media}) => {
         extraSmall: {width: '100%', pad: '0 0 20px'},
         small: {width: '100%' , pad: '0 0 20px'},
         medium: {width: '100%' , pad: '20px 0'},
-        large: {width: '40%', pad: '20px'},
-        infinity: {width: '40%', pad: '20px'}
+        large: {width: '40%', pad: '20px 0'},
+        infinity: {width: '40%', pad: '20px 0'}
     }
     return (
         <Box width={layout[media] ? layout[media].width : '100%'} pad="medium">
@@ -19,7 +19,9 @@ const FooterDescription = ({t, media}) => {
                 {t("footer.description")}
             </Paragraph>
             <Box pad={{"horizontal": media === "large" || media === "infinity" ? "20px 0px" : "0"}}>
-                <Instagram />
+                <a href="http://www.instagram.com/dakcoffeeroasters" target="_blank" rel="noopener noreferrer">
+                    <Instagram />
+                </a>
             </Box>
         </Box>
     );
