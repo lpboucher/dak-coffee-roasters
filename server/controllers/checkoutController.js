59b5a1d64cb6a17db056c3daeb053c19fa37a620
LP Boucher <lp_boucher@hotmail.com>
2019-09-28 19:07:26 +0200

    fix bug in cart only checking out shipping

--- server/controllers/checkoutController.js
+++ server/controllers/checkoutController.js
@@ -1,23 +1,24 @@
 const MoltinGateway = require('@moltin/sdk').gateway
+const MemStorage = require('@moltin/sdk').MemoryStorageFactory
 const tax = require('../utils/taxes');
 const isoCodes = require('../utils/countryCodes');
 const shipping = require('../utils/shippingCost');
 const emails = require('../utils/emails');
- 
-const Moltin = MoltinGateway({
+
+const config = {
   client_id: process.env.MOLTIN_CLIENT_ID,
-  client_secret: process.env.MOLTIN_CLIENT_SECRET
-})
+  client_secret: process.env.MOLTIN_CLIENT_SECRET,
+}
+
+const withStorage = {
+  ...config,
+  storage: new MemStorage()
+}
 
 module.exports = {
     submitOrder: async (req, res, next) => {
         const { customerId, shipping_address, billing_address, items, total } = req.body;
         const { country, county, postcode } = shipping_address;
-        const moltinHeaders = {
-            client_id: process.env.MOLTIN_CLIENT_ID,
-            client_secret: process.env.MOLTIN_CLIENT_SECRET,
-            currency: total.without_tax.currency
-        }
         try {
           console.log('API ORDER SUBMITTING----------', {customerId, shipping_address, billing_address, items, total} );
           const existingShipping = items.find(item => item.name === "shipping");
@@ -30,11 +31,11 @@ module.exports = {
             items
           );
           if(existingShipping && existingShipping.unit_price.amount !== shipCosts * 100) {
-            await MoltinGateway(moltinHeaders).Cart().RemoveItem(existingShipping.id);
+            await MoltinGateway({...withStorage, currency: total.without_tax.currency}).Cart().RemoveItem(existingShipping.id);
           } else if (!existingShipping) {
-            await MoltinGateway(moltinHeaders).Cart().AddCustomItem({name: "shipping", quantity: 1, price: {amount: shipCosts * 100}});
+            await MoltinGateway({...withStorage, currency: total.without_tax.currency}).Cart().AddCustomItem({name: "shipping", quantity: 1, price: {amount: shipCosts * 100}});
           }
-          const order = await MoltinGateway(moltinHeaders).Cart().Checkout(customerId, shipping_address, billing_address);
+          const order = await MoltinGateway({...withStorage, currency: total.without_tax.currency}).Cart().Checkout(customerId, shipping_address, billing_address);
           console.log('API RESPONSE SUBMIT ORDER----------', order);
           console.log(taxes)
           res.json({order, taxes});
